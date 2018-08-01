341de3d5b682b607f8d140959da4baf70972d3aa
Tamas Kozmer <kozmi55@gmail.com>
2018-08-01 16:44:50 +0200

    Make BindableAdapter more generic

--- app/src/main/java/hu/tamaskozmer/bindableadapterexample/BindingUtils.kt
+++ app/src/main/java/hu/tamaskozmer/bindableadapterexample/BindingUtils.kt
@@ -4,7 +4,7 @@ import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
 
 @BindingAdapter("data")
-fun <T> setRecyclerViewProperties(recyclerView: RecyclerView, items: List<T>) {
+fun <T> setRecyclerViewProperties(recyclerView: RecyclerView, items: T) {
     if (recyclerView.adapter is BindableAdapter<*>) {
         (recyclerView.adapter as BindableAdapter<T>).setData(items)
     }
